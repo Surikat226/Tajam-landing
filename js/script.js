5027bdbdf7d7d28e0e1c392ccbded099735c87af
Surikat228 <awf2014@yandex.ru>
2021-05-24 21:22:33 +0300

    Добавил слик-слайдер в верхний блок
    Осталось перенести точи под кнопку, но пока не получается

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function () {
 	$('.header__burger').click(function () {
 		$('.header__burger,.header__links').toggleClass('active');
 	})
+
+
+	$('.slider').slick({
+		arrows: false,
+		dots: true
+	});
+
+
+	//$('.slick-dots').appendTo('.welcome__link')
 })
